c079d0fb6932d8f9319c4f69720075c7b63b79a5
gauravv-kaushik <gkaushik341@gmail.com>
2026-01-19 15:32:12 +0530

    ring size

--- src/screens/RingSizeScreen/index.tsx
+++ src/screens/RingSizeScreen/index.tsx
@@ -1,14 +1,99 @@
-import {StyleSheet, Text, View} from 'react-native';
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import {View, Text, StyleSheet, Dimensions, PanResponder} from 'react-native';
+
+const {width} = Dimensions.get('window');
 
 const RingSizeScreen = () => {
+  const pxPerMM = 8; // <-- this MUST come from calibration screen
+
+  const [diameterMM, setDiameterMM] = useState(8);
+
+  const diameterPx = diameterMM * pxPerMM;
+
+  const panResponder = PanResponder.create({
+    onMoveShouldSetPanResponder: () => true,
+    onPanResponderMove: (_, gesture) => {
+      // const mm = Math.max(10, Math.min(25, diameterMM + gesture.dx / pxPerMM));
+      const rawMM = diameterMM + gesture.dx / pxPerMM;
+      const mm = Math.max(8, Math.min(40, Math.round(rawMM)));
+      setDiameterMM(Number(mm.toFixed(2)));
+    },
+  });
+
   return (
-    <View>
-      <Text>RingSizeScreen</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>Ring Size</Text>
+      <Text style={styles.subtitle}>Place your ring here</Text>
+
+      <View style={styles.centerArea}>
+        <View
+          style={[
+            styles.circle,
+            {
+              width: diameterPx,
+              height: diameterPx,
+              borderRadius: diameterPx / 2,
+            },
+          ]}
+        />
+      </View>
+
+      <Text style={styles.mmText}>{diameterMM} mm</Text>
+
+      <View {...panResponder.panHandlers} style={styles.slider}>
+        <Text
+          style={{
+            color: '#fff',
+            textAlign: 'center',
+            fontSize: 16,
+            fontWeight: 'bold',
+          }}>
+          Slide Left and Right
+        </Text>
+      </View>
     </View>
   );
 };
 
 export default RingSizeScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#1c1f24',
+    alignItems: 'center',
+    paddingTop: 60,
+  },
+  title: {
+    color: '#fff',
+    fontSize: 22,
+    fontWeight: '600',
+  },
+  subtitle: {
+    color: '#aaa',
+    marginTop: 8,
+  },
+  centerArea: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  circle: {
+    borderWidth: 2,
+    borderColor: '#7CFC00',
+  },
+  mmText: {
+    color: '#fff',
+    fontSize: 28,
+    marginBottom: 30,
+  },
+  slider: {
+    width: width * 0.8,
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: 40,
+    backgroundColor: '#8B0000',
+    borderRadius: 20,
+    marginBottom: 40,
+  },
+});
